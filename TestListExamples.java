d73fccc1becfd78f9a03c21f32b890af3cd06c09
slayyden <erikek2014@gmail.com>
2023-02-16 13:50:17 -0800

    made the script

--- TestListExamples.java
+++ TestListExamples.java
@@ -9,7 +9,27 @@ class IsMoon implements StringChecker {
   }
 }
 
+
+
 public class TestListExamples {
+  public class containsCharacter implements StringChecker {
+      char character;
+      public containsCharacter (char character) {
+          this.character = character;
+      }
+
+      @Override
+      public boolean checkString(String s) {
+          for (int i = 0; i < s.length(); ++i) {
+              if (s.charAt(i) == character) {
+                  return true;
+              }
+          }
+
+          return false;
+      }
+  }
+
   @Test(timeout = 500)
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
@@ -18,4 +38,31 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test
+    public void filterTest () {
+        List<String> input1 = 
+            Arrays.asList(new String[]{"Apple", "Banana", "Carrot", "Dog"});
+        StringChecker checkForA = new containsCharacter('a');
+        
+        String[] expected = {"Banana", "Carrot"};
+        assertArrayEquals(expected, ListExamples.filter(input1, checkForA).toArray());
+    }
+
+    @Test
+    public void mergeTest() {
+        List<String> input1 = 
+            Arrays.asList(new String[]{"Apple", "Carrot"});
+        List<String> input2 = 
+            Arrays.asList(new String[]{"Banana", "Dog"});
+        
+        List<String> expected1 = Arrays.asList(new String[]{"Apple", "Banana", "Carrot", "Dog"});
+        List<String> merged = ListExamples.merge(input1, input2);
+
+        for (int i = 0; i < expected1.size(); ++i) {
+            assertEquals(expected1.get(i), merged.get(i));
+        }
+    }
+
+    
 }
--- grade.sh
+++ grade.sh
@@ -1,5 +1,60 @@
+set -e
+
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
+rm -rf ListExamples.java
+rm -rf ListExamples.class
 git clone $1 student-submission
 echo 'Finished cloning'
+
+STUDENT_CODE=student-submission/ListExamples.java
+
+if [[ -f $STUDENT_CODE ]]
+then
+    echo 'Correct java file found.'
+else 
+    echo 'Java file "ListExamples.java" not found'
+    exit 1
+fi
+
+cp $STUDENT_CODE ./
+
+set +e
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> java_compile.txt
+
+COMP_ERRORS=$(wc -l < "java_compile.txt")
+
+# echo $COMP_ERRORS
+
+if [[ $COMP_ERRORS -gt 0 ]]
+then
+    echo "Java file could not compile"
+    exit 1
+fi 
+
+# echo hi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > junit.txt
+sed '2q;d' junit.txt > line_2.txt
+LINE_2=line_2.txt
+
+grep -o E $LINE_2 > failures.txt
+
+grep -o '.' $LINE_2 > total_chars.txt
+
+FAILED_TESTS=$(wc -l < "failures.txt")
+
+TOTAL_CHARS=$(wc -l < "total_chars.txt")
+
+let "RAN_TESTS = $TOTAL_CHARS-$FAILED_TESTS"
+let "PASSED_TESTS = $RAN_TESTS - $FAILED_TESTS"
+# echo $TOTAL_CHARS
+# echo $FAILED_TESTS
+# echo $RAN_TESTS
+
+echo "You passed" $PASSED_TESTS "/" $RAN_TESTS
+
+
+
+
